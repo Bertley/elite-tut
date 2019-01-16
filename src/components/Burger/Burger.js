c4e7c9886b21e2deedf888111bf2c22d831e8c62
ehis <eidialu@elitedigital.ca>
2019-01-16 11:07:34 -0500

    01162019

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// Styles 
+import classes from './Burger.css'
+
+// Components 
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+
+const burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="bread-bottom"/>
+
+        </div>
+    )
+}
+export default burger
